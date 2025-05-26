59cec4708f14ca8d79ab1ec20834491da9b40593
abderrahmen <abderrahmenmalouche12@gmail.com>
2025-05-26 13:50:49 +0200

    added the error handler and the models and the connection with the database

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import express from "express";
 import {PORT} from "./config/env.js";
-
+import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.router.js";
 import subRouter from "./routes/sub.router.js";
 import authRouter from "./routes/auth.router.js";
 
+import errorMiddleware from "./middlewares/error.middleware.js";
+import connectToDatabase from "./database/mongodb.js";
+
 const app=express();
+
+app.use(errorMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 app.get("/",(req,res)=>{
     res.send("hello world");
@@ -15,8 +23,10 @@ app.use("/api/v1/user",userRouter);
 app.use("/api/v1/sub",subRouter);
 
 
-app.listen(PORT,()=>{
+
+app.listen(PORT,async()=>{
     console.log(`Server is running on port ${PORT}`);
+    await connectToDatabase();
 })
 
 export default app;
---
+++ models/subscription.model.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Subscription name is required."],
+        trim: true,
+        minLength: 3,
+        maxLength: 50,
+    },
+    price: {
+        type: Number,
+        required: [true, "Price is required."],
+        min: [0, "Price cannot be negative."],
+        max: [10000, "Price cannot exceed 10000."],
+    },
+    currency: {
+        type: String,
+        required: [true, "Currency is required."],
+        trim: true,
+        enum: ["USD", "EUR", "HUF", "TND", "GBP"],
+        default: "EUR"
+    },
+    frequency: {
+        type: string,
+        required: [true, "Recurrence is required."],
+        enum: ["daily", "monthly", "yearly"],
+        default: "monthly"
+    },
+    paymentMethod: {
+        type: String,
+        required: [true, "Payment method is required."],
+        enum: ["card", "paypal", "bank_transfer"],
+        default: "card",
+        trim: true
+    },
+    type: {
+        type: string,
+        required: [true, "Subscription type is required."],
+        enum: ["Sports", "Entertainment", "News", "Education", "Health"],
+        default: "Entertainment",
+    },
+    status: {
+        type: String,
+        required: [true, "Subscription status is required."],
+        enum: ["active", "inactive", "cancelled"],
+        default: "active",
+    },
+    startDate: {
+        type: Date,
+        required: [true, "Start date is required."],
+        validate: {
+            validator: (value) => value <= new Date(),
+            message: "Start date cannot be in the future."
+        }
+    },
+    renewalDate: {
+        type: Date,
+        validate: {
+            validator: function (value) {
+                return value >= this.startDate;
+            },
+            message: "Renewal Date should be after the Start Date."
+        }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "User ID is required."],
+        index: true
+    }
+
+}, { timestamps: true });
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+subscriptionSchema.pre("save", function (next) {
+    if (!this.renewalDate) {
+        const renewalPeriods = {
+            daily: 1,
+            monthly: 30,
+            yearly: 365
+        }
+        this.renewalDate = new Date(this.startDate.getDate() + renewalPeriods[this.frequency]);
+    }
+    if (this.renewalDate < new Date()) {
+        this.status = "expired";
+    }
+    next(); 
+
+})
+
+export default Subscription;
+
