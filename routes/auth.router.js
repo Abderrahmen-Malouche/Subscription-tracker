1c6be9f97a09f930f77882c9b420223fdd769f97
abderrahmen <abderrahmenmalouche12@gmail.com>
2025-05-26 16:26:31 +0200

    created some controllers

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,8 +1,8 @@
 import {Router} from 'express';
-
+import {login}  from '../controllers/auth.controller.js';
 const authRouter = Router();
 
-authRouter.get("/login",(req,res)=>res.send({title:"Login Successful!"}));
+authRouter.get("/login",login);
 
 authRouter.get("/sign-up",(req,res)=>res.send({title:"Sign up successful!"}));
 
